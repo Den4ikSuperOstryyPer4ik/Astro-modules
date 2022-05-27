a1db38fe94221f6c6e56a042a655a52635599807
Den4ikSuperOstryyPer4ik <102206474+Den4ikSuperOstryyPer4ik@users.noreply.github.com>
2022-05-27 18:57:43 +0300

    Update Music.py

--- Music.py
+++ Music.py
@@ -292,13 +292,13 @@ class LyricsMod(loader.Module):
             await message.delete() 
             await message.client.send_file(message.to_id, music[0].result.document, reply_to=reply.id if reply else None) 
         except: return await message.client.send_message(message.chat_id, f"<b>Музыка с названием <code>{args}</code> не найдена.</b>")
-async def smx2cmd(self, message):
+    async def smx2cmd(self, message):
         """Используй: .smx2 «название», чтобы найти музыку по названию.""" 
         args = utils.get_args_raw(message) 
         reply = await message.get_reply_message() 
         if not args: 
             return await message.edit("<b>Нету аргументов.</b>")  
-        try: 
+        try:
             await message.edit("<b>Загрузка...</b>") 
             music = await message.client.inline_query('@Den4ikSOPer4ik_music_bot', args) 
             await message.delete() 
