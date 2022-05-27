2acf188fb614547f983a767d6a5410660c7ad0cd
Den4ikSuperOstryyPer4ik <102206474+Den4ikSuperOstryyPer4ik@users.noreply.github.com>
2022-05-27 19:07:53 +0300

    Create Musicx2.py

---
+++ Musicx2.py
@@ -0,0 +1,26 @@
+# requires: requests bs4 
+# meta developer: @ToXicUse, @Den4ikSuperOstryyPer4ik
+
+from bs4 import BeautifulSoup
+import requests
+from telethon.tl.types import Message
+from telethon.tl.functions.channels import JoinChannelRequest
+from ..utils import rand
+from urllib.parse import quote_plus
+from .. import loader  # noqa
+from .. import utils  # noqa
+import logging
+import re
+
+    async def smx2cmd(self, message):
+        """Используй: .smx2 «название», чтобы найти музыку по названию.""" 
+        args = utils.get_args_raw(message) 
+        reply = await message.get_reply_message() 
+        if not args: 
+            return await message.edit("<b>Нету аргументов.</b>")  
+        try:
+            await message.edit("<b>Загрузка...</b>") 
+            music = await message.client.inline_query('@Den4ikSOPer4ik_music_bot', args) 
+            await message.delete() 
+            await message.client.send_file(message.to_id, music[0].result.document, reply_to=reply.id if reply else None) 
+        except: return await message.client.send_message(message.chat_id, f"<b>Музыка с названием <code>{args}</code> не найдена.</b>")
