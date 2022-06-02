0aa133f8fc4d8e0f1be0861a2622f933ff5e8032
Den4ikSuperOstryyPer4ik <102206474+Den4ikSuperOstryyPer4ik@users.noreply.github.com>
2022-06-02 09:35:14 +0300

    Update LikeAndTrollx2.py

--- LikeAndTrollx2.py
+++ LikeAndTrollx2.py
@@ -3,7 +3,7 @@ from .. import loader
 class LikeAntTrollMod(loader.Module):
 	"""улучшенная версия предыдущего модуля"""
 	strings = {"name": "Compliments and Toxicity(beta)"}
-	Комплименты = ["Ты...прекрасная, а еще....", "красивая", "заботливая", "добрая"]
 	async def likezpmx2cmd(self, message): 
+		Комплименты = ["Ты...прекрасная, а еще....", "красивая", "заботливая", "добрая"]
 		for i in range(len(Комплименты)): 
 			await message.respond(Комплименты[i])
