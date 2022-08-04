665ae1c47adde3be3a915a435e388758f2f586eb
Den4ikSuperOstryyPer4ik <102206474+Den4ikSuperOstryyPer4ik@users.noreply.github.com>
2022-08-04 11:41:42 +0300

    Create commands_logger.py
    new module

---
+++ commands_logger.py
@@ -0,0 +1,95 @@
+#               _             __  __           _       _           
+#     /\       | |           |  \/  |         | |     | |          
+#    /  \   ___| |_ _ __ ___ | \  / | ___   __| |_   _| | ___  ___ 
+#   / /\ \ / __| __| '__/ _ \| |\/| |/ _ \ / _` | | | | |/ _ \/ __|
+#  / ____ \\__ \ |_| | | (_) | |  | | (_) | (_| | |_| | |  __/\__ \
+# /_/    \_\___/\__|_|  \___/|_|  |_|\___/ \__,_|\__,_|_|\___||___/
+#
+#               © Copyright 2022
+#
+#      https://t.me/Den4ikSuperOstryyPer4ik
+#                      and
+#             https://t.me/ToXicUse
+#
+#       🔒 Licensed under the GNU AGPLv3
+#    https://www.gnu.org/licenses/agpl-3.0.html
+#
+# meta developer: @AstroModules
+# meta pic: https://0x0.st/oeWa.jpg
+# meta banner: 
+# scope: hikka_only
+# scope: hikka_min 1.3.0
+
+from .. import loader, utils as u
+import logging
+from telethon.tl.functions.channels import InviteToChannelRequest
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class HikkaCommandsLoggerMod(loader.Module):
+    """Hikka Commands Logger"""
+    strings = {
+        "name": "HikkaCommandsLogger",
+        "log": "<b>Command:\n« <code>{}</code> »\n\nFrom --> {}\nIn {}\nCommand Message Link --> <a href='https://t.me/c/{}/{}'>CLICK</a></b>",
+    }
+
+    async def client_ready(self, client, db):
+        logger.warning('Hikka Commangs Logging installed!')
+        self.chat_l, _ = await u.asset_channel(
+            self.client,
+            "hikka-commands-logs",
+            "💬 Chat for Hikka Commands LOGGER :) 🔳",
+            silent=True,
+            avatar="https://0x0.st/oep0.jpg",
+        )
+
+        self.chat_logs = f"-100{self.chat_l.id}"
+        logger.info("Привет от t.me/AstroModules :)")
+
+    @loader.watcher()
+    @loader.tag(only_commands=True)
+    async def watcher(self, message):
+        sender = await message.get_sender()
+
+        if not sender.username:
+            user_link = f"<a href=tg://user?id={sender.id}>{sender.first_name}</a>"
+        else:
+            user_link = f"<a href='https://t.me/{sender.username}'>{sender.first_name}</a>"
+
+        if message.is_private:
+            chat_title = "PM"
+        else:
+            chat = await self._client.get_entity(message.peer_id)
+            chat_title = chat.title
+            if not chat.username:
+                chat_link = f"CHAT: {chat_title}"
+            else:
+                chat_link = f"CHAT: <a href='https://t.me/{chat.username}'>{chat_title}</a>"
+            
+            chat_title = chat.title
+
+
+        async def send():
+            await self.inline.bot.send_message(
+                self.chat_logs,
+                self.strings("log").format(
+                    message.raw_text,
+                    user_link,
+                    chat_link,
+                    chat.id,
+                    message.id
+                ),
+                disable_web_page_preview=True,
+                parse_mode="HTML",
+            )
+
+        try:
+            await send()
+        except Exception:
+            await self._client(
+                InviteToChannelRequest(
+                    self.chat_l,
+                    [self.inline.bot_username],
+                )
+            )
+            await send()
