a12a23f5e0e30643a4a5d1fbbeddda9580fd1135
ToXic2290 <derzkiy8233@gmail.com>
2023-01-30 21:42:49 +0600

    Update astroafk.py
    Я гей

--- astroafk.py
+++ astroafk.py
@@ -106,7 +106,7 @@ class AstroAfkMod(loader.Module):
 				lambda: self.strings("_cfg_cst_btn"),
 				validator=loader.validators.Union(
 					loader.validators.Series(fixed_len=2),
-					loader.validators.NoneType(),
+					loader.validators.NoneType()
 				),
 			),
 			loader.ConfigValue(
